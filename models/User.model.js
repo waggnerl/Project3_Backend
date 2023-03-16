0e21ac5b4ddae80b57d21a9d517405b05c4f33cf
waggnerl <wagnermflira@gmail.com>
2023-03-16 11:44:24 +0000

    featÇULTIMATE CHANGE

--- models/User.model.js
+++ models/User.model.js
@@ -17,8 +17,11 @@ const userSchema = new Schema(
       required: [true, "Password is required."],
     },
     role: { type: String, enum: ["personal", "student"], required: true },
-    img: { type: String, 
-    default: "../public/profile.png"},
+    img: {
+      type: String,
+      default:
+        "https://images.unsplash.com/photo-1601046668428-94ea13437736?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=2167&q=80",
+    },
     students: [
       {
         type: Schema.Types.ObjectId,
